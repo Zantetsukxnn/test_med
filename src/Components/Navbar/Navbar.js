dd14f5f05853096b1d04c934b5d4b554ec07af6f
Zantetsukxnn <ambert0130@gmail.com>
2025-07-29 19:21:45 -0400

    Added Navbar and Landing Page component

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <>
+    <nav>
+    {/* Navigation logo section */}
+    <div className="nav__logo">
+      {/* Link to the home page */}
+      <a href="/">
+        <img
+          src="https://img.icons8.com/?size=192&id=5359&format=png"
+          alt="caduceus"
+          width={50}
+          height={50}
+        />
+        StayHealthy
+      </a>
+      {/* A span element for styling purposes */}
+    </div>
+    {/* Navigation icon section with an onClick event listener */}
+    <div className="nav__icon" onclick="{handleClick}">
+      {/* Font Awesome icon for bars (hamburger menu) */}
+      <i className="fa fa-times fa fa-bars" />
+    </div>
+    {/* Unordered list for navigation links with 'active' class */}
+    <ul className="nav__links active">
+      {/* List item for the 'Home' link */}
+      <li className="link">
+        <a href="../Landing_Page/LandingPage.html">Home</a>
+      </li>
+      {/* List item for the 'Appointments' link */}
+      <li className="link">
+        <a href="#">Appointments</a>
+      </li>
+      {/* List item for the 'Sign Up' link with a button */}
+      <li className="link">
+        <a href="../Sign_Up/Sign_Up.html">
+          <button className="btn1">Sign Up</button>
+        </a>
+      </li>
+      {/* List item for the 'Login' link with a button */}
+      <li className="link">
+        <a href="../Login/Login.html">
+          <button className="btn1">Login</button>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  </>
+  );
+}
+
+export default Navbar;
